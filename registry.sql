41504e70871c0ded004993e39aa78e53073738d7
JdoesTech <onyangoocholla1139@gmail.com>
2025-09-23 21:01:17 +0300

    Addition

--- registry.sql
+++ registry.sql
@@ -3,7 +3,7 @@ Create Database land_registry;
 use land_registry;
 
 create table owners(
-ID int primary key,
+ownerID int primary key,
 owner_name varchar(100) not null,
 Phone int(10) not null,
 email varchar(50) not null, 
@@ -11,14 +11,14 @@ date_of_Birth date not null
 );
 
 create table land (
-ID binary(16) primary key,
+landID binary(16) primary key,
 title varchar(100) not null,
 date_of_purchase datetime not null,
-price_of_purchase float(2) check (price_of_purchase >= 0),
+price_of_purchase decimal(12,2) check (price_of_purchase >= 0),
 ownerID int not null, 
-foreign key (ownerID) references owners(ID) 
-	ON DELETE CASCADE
-        ON UPDATE CASCADE,
+foreign key (ownerID) references owners(ownerID) 
+	on delete cascade
+    on delete cascade
 );
 
 create table buyers(
@@ -35,11 +35,11 @@ date_of_first_enquiry datetime not null,
 buyerID int not null,
 landID binary(16) not null,
 foreign key (buyerID) references buyers(buyerID),
-	ON DELETE CASCADE
-        ON UPDATE CASCADE,
-foreign key (landID) references land(ID)
-	ON DELETE CASCADE
-        ON UPDATE CASCADE,
+    on delete cascade
+    on delete cascade,
+foreign key (landID) references land(landID)
+    on delete cascade
+    on delete cascade
 );
 
 create index idx_land_owner_id on land(ownerID);
